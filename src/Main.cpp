ccc9fae930b6d61fee6633b777791b50d2c381bc
Julian Steiner <juli.steiner@bluewin.ch>
2022-04-24 12:59:16 +0200

    Removed transform class

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,15 +2,17 @@
 #include "buffer/IndexBuffer.h"
 #include "buffer/VertexBuffer.h"
 #include "glm/detail/qualifier.hpp"
+#include "glm/ext/matrix_clip_space.hpp"
 #include "glm/ext/matrix_transform.hpp"
+#include "glm/ext/quaternion_transform.hpp"
 #include "glm/ext/vector_float3.hpp"
 #include "glm/ext/vector_float4.hpp"
+#include "glm/gtc/quaternion.hpp"
 #include "glm/gtx/string_cast.hpp"
 #include "shader/FragmentShader.h"
 #include "shader/Shader.h"
 #include "shader/ShaderProgram.h"
 #include "shader/VertexShader.h"
-#include "transform/Transform.h"
 #include "vertexArrayObject/VertexArrayObject.h"
 #include <array>
 #include <vector>
@@ -55,15 +57,19 @@ int main() {
 
   glfwSetWindowSizeCallback(window, framebuffer_size_callback);
 
+  glEnable(GL_DEPTH_TEST);
+
   const char *vertexShaderSourceCode =
       "#version 330 core\n"
       "layout (location = 0) in vec3 aPos;\n"
       "layout (location = 1) in vec2 texCoord;\n"
       "out vec2 textureCoord;\n"
-      "uniform mat4 transform;\n"
+      "uniform mat4 model;\n"
+      "uniform mat4 view;\n"
+      "uniform mat4 projection;\n"
       "void main()\n"
       "{\n"
-      " gl_Position = transform * vec4(aPos, 1.0);\n"
+      " gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
       " textureCoord = texCoord;\n"
       "}\0";
 
@@ -86,63 +92,82 @@ int main() {
   shaderProgram.addShader(vertexShader);
   shaderProgram.addShader(fragmentShader);
 
-  struct Vertex{
-    glm::vec3 position;
-    glm::vec2 texCoord;
-  };
+  std::array<float, 5 * 36> verteces = {
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-  Vertex upRight;
-  upRight.position = glm::vec3(0.5, 0.5, 0.0);
-  upRight.texCoord = glm::vec2(1, 1);
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
 
-  Vertex downRight;
-  downRight.position = glm::vec3(0.5, -0.5, 0.0);
-  downRight.texCoord = glm::vec2(1, 0);
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
 
-  Vertex upLeft;
-  upLeft.position = glm::vec3(-0.5, 0.5, 0.0);
-  upLeft.texCoord = glm::vec2(0, 1);
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
 
-  Vertex downLeft;
-  downLeft.position = glm::vec3(-0.5, -0.5, 0.0);
-  downLeft.texCoord = glm::vec2(0, 0);
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
 
-  std::array<Vertex, 4> verteces = {upRight, downRight, upLeft, downLeft};
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f};
 
   buffer::VertexBuffer vertexBuffer = buffer::VertexBuffer();
   vertexBuffer.loadData(verteces, GL_STATIC_DRAW);
   vertexBuffer.addAttribute(buffer::BufferAttribute(GL_FLOAT, 3));
   vertexBuffer.addAttribute(buffer::BufferAttribute(GL_FLOAT, 2));
 
-  std::array<unsigned int, 6> elements = {0, 1, 2, 2, 3, 1};
-  buffer::IndexBuffer indexBuffer = buffer::IndexBuffer();
-  indexBuffer.loadData(elements, GL_STATIC_DRAW);
-
   // Vertex Array Object
   vertexArrayObject::VertexArrayObject vao = vertexArrayObject::VertexArrayObject();
   vao.addBuffer(vertexBuffer);
-  vao.addBuffer(indexBuffer);
 
-  texture::Texture tex = texture::Texture2D("texture2.jpg");
+  texture::Texture tex = texture::Texture2D("texture.jpg");
 
-  // Transformations
-  transform::Transform trans;
-  trans.addScale(glm::vec3(0.5f, 0.5f, 0.0f));
-  trans.addRotation(glm::radians(50.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+  glm::mat4 model = glm::mat4(1.0f);
+  model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
-  std::cout << glm::to_string(trans.getMatrix()) << std::endl;
+  glm::mat4 viewMatrix = glm::mat4(1.0f);
+  viewMatrix = glm::translate(viewMatrix, glm::vec3(0.0f, 0.0f, -3.0f));
+
+  glm::mat4 projection = glm::perspective(glm::radians(60.0f), 1000.0f / 1000.0f, 0.1f, 100.0f);
 
   do {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    model = glm::rotate(glm::mat4(1.0f), (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));  
+
     shaderProgram.bind();
-    shaderProgram.setUniformMatrix4x4("transform", trans.getMatrix());
+    shaderProgram.setUniformMatrix4x4("model", model);
+    shaderProgram.setUniformMatrix4x4("view", viewMatrix);
+    shaderProgram.setUniformMatrix4x4("projection", projection);
 
     tex.bind();
-  
     vao.bind();
 
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
